6852c388ef5c8b3c1d1bf3aa907765c0341fd177
Ben Cowden <benjcowden@gmail.com>
2020-08-17 01:44:57 -0700

    Finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// This file won't be included when this is put on Author.
+
 import * as CopyCatContainer from './containers/CopyCatContainer';
 
 document.addEventListener('DOMContentLoaded', () => CopyCatContainer);
