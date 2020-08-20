ba6b2460843630bd5171ffd1122a2527de20a2f7
Ben Cowden <benjcowden@gmail.com>
2020-08-20 14:56:53 -0700

    Update with suggested changes

--- src/components/CopyCat.js
+++ src/components/CopyCat.js
@@ -6,7 +6,7 @@ export class CopyCat extends React.Component {
     render() {
         return (
             <div style={styles.divStyles}>
-                <h1 style={{ marginBottom: 80 }}>{`Copy Cat ${this.props.name || 'Tom'}`}</h1>
+                <h1 style={{ marginBottom: 80 }}>Copy Cat {this.props.name || 'Tom'}</h1>
                 <input
                     type="text"
                     value={this.props.value}
